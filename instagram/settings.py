4034ee0004c6ba2faea3bfa5cfe7688d6533d144
Levy <leskeylevy@gmail.com>
2018-10-08 07:54:08 +0300

    changed setting to acccount for static files

--- instagram/settings.py
+++ instagram/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
